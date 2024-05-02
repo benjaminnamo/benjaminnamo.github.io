e9ef882b0ac46026b6bac46d0c7e42c8c4a8b935
bnamo <80124875+benjaminnamo@users.noreply.github.com>
2024-05-02 18:25:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    function randomFlicker() {
-        const flickerCount = Math.floor(Math.random() * 4) + 3; // 3 to 6 flickers per occurrence 
-        let currentFlicker = 0;
-
-        function triggerFlicker() {
-            const button = document.querySelector('#playButton a');
-            button.style.animation = 'none'; // Reset animation to clear previous settings
-            button.style.animation = 'flicker-effect 0.1s ease-in-out forwards'; // Apply flickering effect
-            
-            if (++currentFlicker < flickerCount) {
-                setTimeout(triggerFlicker, 100); // Trigger next flicker after 100 ms
-            } else {
-                setTimeout(() => {
-                    button.style.animation = 'none'; // Clear the animation after the last flicker
-                }, 100);
-            }
-        }
-
-        triggerFlicker(); // Start the flickering effect
-
-        const nextFlickerDelay = Math.floor(Math.random() * (10000 - 3000 + 1) + 3000); // 3 to 10 seconds until next flicker session
-        setTimeout(randomFlicker, nextFlickerDelay); // Schedule the next round of flickers
-    }
-
-    randomFlicker(); // Start the flicker effect loop
-});
-
-
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
